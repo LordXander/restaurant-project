3d2bd8430b3b570d83154c0ddb4e5bb461c97054
Xander <andreipetric@gmail.com>
2018-07-31 17:42:19 +0300

    main_update

--- js/main.js
+++ js/main.js
@@ -74,11 +74,11 @@ fillCuisinesHTML = (cuisines = self.cuisines) => {
 initMap = () => {
   self.newMap = L.map('map', {
         center: [40.722216, -73.987501],
-        zoom: 12,
+        zoom: 10,
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: 'pk.eyJ1IjoiZGFuaWVsYS1rdWVzdGVyIiwiYSI6ImNqam9jdjF1YTAxczQzcG9na3JrZmh2bnkifQ.zMFUT-fI5tsviUOPZsJxog',
+    mapboxToken: 'pk.eyJ1IjoibG9yZHhhbmRlciIsImEiOiJjams5cmJwbTAycHVkM2tvMmI2YTRkaGdnIn0.k2mLtIvHa93c4MBueu99Uw',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
